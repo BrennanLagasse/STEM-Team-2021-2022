5a04bed5b5d7a1b17c0e763ec8282b2d0f8e14c1
Brennan Lagasse <51427169+BrennanLagasse@users.noreply.github.com>
2021-12-21 13:40:19 -0500

    Update accelerometerTest.ino

--- accelerometerTest.ino
+++ accelerometerTest.ino
@@ -1,53 +1,17 @@
-*
+# //www.sparkfun.com/commerce/categories.php?c=80
 
-  ADXL3xx
-
-  Reads an Analog Devices ADXL3xx accelerometer and communicates the
-
-  acceleration to the computer. The pins used are designed to be easily
-
-  compatible with the breakout boards from SparkFun, available from:
-
-  http://www.sparkfun.com/commerce/categories.php?c=80
-
-  The circuit:
-
-  - analog 0: accelerometer self test
-
-  - analog 1: z-axis
-
-  - analog 2: y-axis
-
-  - analog 3: x-axis
-
-  - analog 4: ground
-
-  - analog 5: vcc
-
-  created 2 Jul 2008
-
-  by David A. Mellis
-
-  modified 30 Aug 2011
-
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/ADXL3xx
-
-*/
-
-// these constants describe the pins. They won't change:
+# analog 0: accelerometer self test
+# analog 1: z-axis
+# analog 2: y-axis
+# analog 3: x-axis
+# analog 4: ground
+# analog 5: vcc
+# http://www.arduino.cc/en/Tutorial/ADXL3xx
 
 const int groundpin = 18;             // analog input pin 4 -- ground
-
 const int powerpin = 19;              // analog input pin 5 -- voltage
-
 const int xpin = A3;                  // x-axis of the accelerometer
-
 const int ypin = A2;                  // y-axis
-
 const int zpin = A1;                  // z-axis (only on 3-axis models)
 
 void setup() {
