76266d5a2e8d0434ca2772777de3573b898a5f53
ZakirUzman <93616696+ZakirUzman@users.noreply.github.com>
2022-01-05 13:21:41 -0500

    Update tracking-algorithm-ip3.ino

--- tracking-algorithm-ip3.ino
+++ tracking-algorithm-ip3.ino
@@ -1,60 +1,3 @@
-int trigPin = 2;
-int echoPin = 3;
-double duration;
-double distance;
-
-
-int numDataPoints = 0;
-double dataLog[1000][2];
-
-
-void search(int duration) {
-
-  // @ Param duration - duration of the search in seconds
-  
-  // Set the ending time to the current time plus the duration in milliseconds
-  int endTime = duration*1000 + millis();
-
-  Serial.println(endTime);
-  Serial.print(millis());
-
-  while(millis() < endTime) {
-    Serial.println(millis());
-  }
-  
-  while(millis() < endTime) {
-    // Collect data
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-
-
-    duration = pulseIn(echoPin, HIGH);
-    distance = duration * 0.0343 /2;
-
-
-    if(distance > 0 && distance < 500) {
-      // Print out distance
-      //Serial.println(distance);
-
-      // Add distance to the log along with a timestamp
-      dataLog[numDataPoints][0] = distance;
-      dataLog[numDataPoints][1] = millis();
-
-      
-      numDataPoints += 1;
-    }
-    else {
-     
-      Serial.println("Value detected out of sensor range.");
-    }
-  }
-  Serial.println("Done searching");
-}
-
-
 
 void setup() {
   // Setup pins
@@ -74,17 +17,16 @@ void setup() {
   
   // Increment through pools
   for(int i = 0; i <= numDataPoints - poolSize; i += poolSize) {
-    double total = 0
-    
+    double total = 0;
     // Increment through elements of the pool and average to a single value
     for(int j = i; j < poolSize + i; j++) {
-      total += dataLog[j,0];
+      total += dataLog[j,0][0];
     }
     
     double poolAverage = total/poolSize;
     
     pooledData[i/poolSize][0] = poolAverage;
-    pooledData[i/poolSize][1] = (dataLog[i][1] + dataLog[i+poolSize-1])/2;
+    pooledData[i/poolSize][1] = (dataLog[i][1] + dataLog[i+poolSize-1][1])/2;
   }
   
   // The optimal landing location is flat and low
@@ -98,7 +40,7 @@ void setup() {
   // Now that data is pooled, search for a section with minimum deviance
   int minDeviance = 500;
   
-  for(int i = 0; i <= poolAverage.size() - landRange; i++) {
+  for(int i = 0; i <= pooledData.size() - landRange; i++) {
     int max = 0;
     int min = 500;
     
